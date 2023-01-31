fc006ed4e4b8bc4e6bcb07f5d46c4f31375c5ecc
Humza <111937011+humza-swe@users.noreply.github.com>
2023-01-31 18:12:22 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,11 +13,11 @@ Create a detailed list of 5 possible issues that the pet could be experiencing.
 
 For each option, include its likelihood as a percentage and also rank it as severe, moderate, or low risk.
 
-At the end, recommend whether or not the pet owner should take the pet to the veterinarian and also include a few relevant questions that the user should ask their pet's veterinarian. 
+At the end, recommend whether or not the pet owner should take the pet to the veterinarian and also include a few relevant questions that the user should ask their pet's veterinarian. Also list any tests that might be required (x-ray, MRI, etc.)
 
 Finally, leave a cute hopeful message for the pet owner about their pet that will make them smile.
 
-Pretend that you are an experienced and skilled veterinarian. Be verbose, and write in the style of Paul Graham. List the output as a table. 
+Pretend that you are an experienced and skilled veterinarian. Be verbose, and write in the style of Paul Graham. 
 
 `
 
